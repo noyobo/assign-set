2dc5c4ee14bfd1e623417a604bb1e7c65bb98c47
noyobo <noyobo@gmail.com>
2022-06-09 11:03:27 +0800

    fix: update value

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,39 +1,120 @@
-const get = require('lodash.get');
 const set = require('../index');
 
-const assignSet = (path, value, obj) => {
-  obj = obj || {};
+const assignSet = (obj, path, value) => {
+  if (typeof obj === 'string') {
+    value = path;
+    path = obj;
+    obj = {};
+  }
+
   set(obj, path, value);
   return obj;
 };
 
+// prettier-ignore
 describe('set', () => {
-  it('should work', () => {
-    let t;
+  it('should set a value when unset', () => {
+    expect(assignSet({}, 'a.b.c', 1)).toEqual({ a: { b: { c: 1 } } });
+    expect(assignSet({}, 'a.b.c', '1')).toEqual({ a: { b: { c: '1' } } });
+    expect(assignSet({}, 'a.b.c', false)).toEqual({ a: { b: { c: false } } });
+    expect(assignSet({}, 'a.b.c', [1])).toEqual({ a: { b: { c: [1] } } });
+    expect(assignSet({}, 'a.b.c', { a: 1 })).toEqual({ a: { b: { c: { a: 1 } } } });
 
-    t = assignSet('a.b.c', 'd');
-    expect(get(t, 'a.b.c')).toBe('d');
+    expect(assignSet({}, 'a.1.c', 1)).toEqual({ a: { 1: { c: 1 } } });
+    expect(assignSet({}, 'a.1.c', '1')).toEqual({ a: { 1: { c: '1' } } });
+    expect(assignSet({}, 'a.1.c', false)).toEqual({ a: { 1: { c: false } } });
+    expect(assignSet({}, 'a.1.c', [1])).toEqual({ a: { 1: { c: [1] } } });
+    expect(assignSet({}, 'a.1.c', { a: 1 })).toEqual({ a: { 1: { c: { a: 1 } } } });
 
-    t = assignSet('a.b.6.c', 'd');
-    expect(get(t, 'a.b')).toEqual({ 6: { c: 'd' } });
+    expect(assignSet({}, 'a[1].c', 1)).toEqual({ a: [undefined, { c: 1 }] });
+    expect(assignSet({}, 'a[1].c', '1')).toEqual({ a: [undefined, { c: '1' }] });
+    expect(assignSet({}, 'a[1].c', false)).toEqual({ a: [undefined, { c: false }] });
+    expect(assignSet({}, 'a[1].c', [1])).toEqual({ a: [undefined, { c: [1] }] });
+    expect(assignSet({}, 'a[1].c', { a: 1 })).toEqual({ a: [undefined, { c: { a: 1 } }] });
 
-    t = assignSet('a[]', 'd');
-    expect(get(t, 'a')).toEqual({ '': 'd' });
+    expect(assignSet({}, 'a[1][1].c', 1)).toEqual({ a: [undefined, [undefined, { c: 1 }]] });
+    expect(assignSet({}, 'a[1][1].c', '1')).toEqual({ a: [undefined, [undefined, { c: '1' }]] });
+    expect(assignSet({}, 'a[1][1].c', false)).toEqual({ a: [undefined, [undefined, { c: false }]] });
+    expect(assignSet({}, 'a[1][1].c', [1])).toEqual({ a: [undefined, [undefined, { c: [1] }]] });
+    expect(assignSet({}, 'a[1][1].c', { a: 1 })).toEqual({ a: [undefined, [undefined, { c: { a: 1 } }]] });
+  });
 
-    t = assignSet('a.b[6].c', 'd');
-    expect(get(t, 'a.b')).toEqual([, , , , , , { c: 'd' }]);
+  it('should update a value on array', () => {
+    expect(assignSet({ a: [] }, 'a.b.c', 1)).toEqual({ a: { b: { c: 1 } } });
+    expect(assignSet({ a: [] }, 'a.b.c', '1')).toEqual({ a: { b: { c: '1' } } });
+    expect(assignSet({ a: [] }, 'a.b.c', false)).toEqual({ a: { b: { c: false } } });
+    expect(assignSet({ a: [] }, 'a.b.c', [1])).toEqual({ a: { b: { c: [1] } } });
+    expect(assignSet({ a: [] }, 'a.b.c', { a: 1 })).toEqual({ a: { b: { c: { a: 1 } } } });
 
-    t = assignSet('a.b["6"].c', 'd');
-    expect(get(t, 'a.b')).toEqual({ 6: { c: 'd' } });
+    expect(assignSet({ a: [] }, 'a.1.c', 1)).toEqual({ a: { 1: { c: 1 } } });
+    expect(assignSet({ a: [] }, 'a.1.c', '1')).toEqual({ a: { 1: { c: '1' } } });
+    expect(assignSet({ a: [] }, 'a.1.c', false)).toEqual({ a: { 1: { c: false } } });
+    expect(assignSet({ a: [] }, 'a.1.c', [1])).toEqual({ a: { 1: { c: [1] } } });
+    expect(assignSet({ a: [] }, 'a.1.c', { a: 1 })).toEqual({ a: { 1: { c: { a: 1 } } } });
 
-    t = assignSet('a.b["a_6"].c', 'd');
-    expect(get(t, 'a.b')).toEqual({ a_6: { c: 'd' } });
+    expect(assignSet({ a: [] }, 'a[1].c', 1)).toEqual({ a: [undefined, { c: 1 }] });
+    expect(assignSet({ a: [] }, 'a[1].c', '1')).toEqual({ a: [undefined, { c: '1' }] });
+    expect(assignSet({ a: [] }, 'a[1].c', false)).toEqual({ a: [undefined, { c: false }] });
+    expect(assignSet({ a: [] }, 'a[1].c', [1])).toEqual({ a: [undefined, { c: [1] }] });
+    expect(assignSet({ a: [] }, 'a[1].c', { a: 1 })).toEqual({ a: [undefined, { c: { a: 1 } }] });
 
-    t = assignSet('a.b["a-[]6"].c', 'd');
-    expect(get(t, 'a.b')).toEqual({ 'a-[]6': { c: 'd' } });
+    expect(assignSet({ a: [] }, 'a[1][1].c', 1)).toEqual({ a: [undefined, [undefined, { c: 1 }]] });
+    expect(assignSet({ a: [] }, 'a[1][1].c', '1')).toEqual({ a: [undefined, [undefined, { c: '1' }]] });
+    expect(assignSet({ a: [] }, 'a[1][1].c', false)).toEqual({ a: [undefined, [undefined, { c: false }]] });
+    expect(assignSet({ a: [] }, 'a[1][1].c', [1])).toEqual({ a: [undefined, [undefined, { c: [1] }]] });
+    expect(assignSet({ a: [] }, 'a[1][1].c', { a: 1 })).toEqual({ a: [undefined, [undefined, { c: { a: 1 } }]] });
+  });
 
-    t = assignSet('a.b[1][2]["b"].c', 'd');
-    expect(get(t, 'a.b')).toEqual([, [, , { b: { c: 'd' } }]]);
+  it('should update a value on object', () => {
+    expect(assignSet({ a: { f: 2 } }, 'a.b.c', 1)).toEqual({ a: { f: 2, b: { c: 1 } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.b.c', '1')).toEqual({ a: { f: 2, b: { c: '1' } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.b.c', false)).toEqual({ a: { f: 2, b: { c: false } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.b.c', [1])).toEqual({ a: { f: 2, b: { c: [1] } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.b.c', { a: 1 })).toEqual({ a: { f: 2, b: { c: { a: 1 } } } });
+
+    expect(assignSet({ a: { f: 2 } }, 'a.1.c', 1)).toEqual({ a: { f: 2, 1: { c: 1 } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.1.c', '1')).toEqual({ a: { f: 2, 1: { c: '1' } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.1.c', false)).toEqual({ a: { f: 2, 1: { c: false } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.1.c', [1])).toEqual({ a: { f: 2, 1: { c: [1] } } });
+    expect(assignSet({ a: { f: 2 } }, 'a.1.c', { a: 1 })).toEqual({ a: { f: 2, 1: { c: { a: 1 } } } });
+
+    expect(assignSet({ a: { f: 2 } }, 'a[1].c', 1)).toEqual({ a: [undefined, { c: 1 }] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1].c', '1')).toEqual({ a: [undefined, { c: '1' }] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1].c', false)).toEqual({ a: [undefined, { c: false }] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1].c', [1])).toEqual({ a: [undefined, { c: [1] }] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1].c', { a: 1 })).toEqual({ a: [undefined, { c: { a: 1 } }] });
+
+    expect(assignSet({ a: { f: 2 } }, 'a[1][1].c', 1)).toEqual({ a: [undefined, [undefined, { c: 1 }]] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1][1].c', '1')).toEqual({ a: [undefined, [undefined, { c: '1' }]] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1][1].c', false)).toEqual({ a: [undefined, [undefined, { c: false }]] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1][1].c', [1])).toEqual({ a: [undefined, [undefined, { c: [1] }]] });
+    expect(assignSet({ a: { f: 2 } }, 'a[1][1].c', { a: 1 })).toEqual({ a: [undefined, [undefined, { c: { a: 1 } }]] });
+  });
+
+  it('should update a value is boolean', () => {
+    expect(assignSet({ a: true }, 'a.b.c', 1)).toEqual({ a: { b: { c: 1 } } });
+    expect(assignSet({ a: true }, 'a.b.c', '1')).toEqual({ a: { b: { c: '1' } } });
+    expect(assignSet({ a: true }, 'a.b.c', false)).toEqual({ a: { b: { c: false } } });
+    expect(assignSet({ a: true }, 'a.b.c', [1])).toEqual({ a: { b: { c: [1] } } });
+    expect(assignSet({ a: true }, 'a.b.c', { a: 1 })).toEqual({ a: { b: { c: { a: 1 } } } });
+
+    expect(assignSet({ a: true }, 'a.1.c', 1)).toEqual({ a: { 1: { c: 1 } } });
+    expect(assignSet({ a: true }, 'a.1.c', '1')).toEqual({ a: { 1: { c: '1' } } });
+    expect(assignSet({ a: true }, 'a.1.c', false)).toEqual({ a: { 1: { c: false } } });
+    expect(assignSet({ a: true }, 'a.1.c', [1])).toEqual({ a: { 1: { c: [1] } } });
+    expect(assignSet({ a: true }, 'a.1.c', { a: 1 })).toEqual({ a: { 1: { c: { a: 1 } } } });
+
+    expect(assignSet({ a: true }, 'a[1].c', 1)).toEqual({ a: [undefined, { c: 1 }] });
+    expect(assignSet({ a: true }, 'a[1].c', '1')).toEqual({ a: [undefined, { c: '1' }] });
+    expect(assignSet({ a: true }, 'a[1].c', false)).toEqual({ a: [undefined, { c: false }] });
+    expect(assignSet({ a: true }, 'a[1].c', [1])).toEqual({ a: [undefined, { c: [1] }] });
+    expect(assignSet({ a: true }, 'a[1].c', { a: 1 })).toEqual({ a: [undefined, { c: { a: 1 } }] });
+
+    expect(assignSet({ a: true }, 'a[1][1].c', 1)).toEqual({ a: [undefined, [undefined, { c: 1 }]] });
+    expect(assignSet({ a: true }, 'a[1][1].c', '1')).toEqual({ a: [undefined, [undefined, { c: '1' }]] });
+    expect(assignSet({ a: true }, 'a[1][1].c', false)).toEqual({ a: [undefined, [undefined, { c: false }]] });
+    expect(assignSet({ a: true }, 'a[1][1].c', [1])).toEqual({ a: [undefined, [undefined, { c: [1] }]] });
+    expect(assignSet({ a: true }, 'a[1][1].c', { a: 1 })).toEqual({ a: [undefined, [undefined, { c: { a: 1 } }]] });
   });
 
   it('should assign to array', () => {
